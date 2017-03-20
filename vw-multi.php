bceb6ae3664145a40cfa4615a018341f5dd42a21
jumaha25 <mshoestrng@posteo.de>
2017-03-20 17:02:49 +0100

    Update vw-multi.php

--- vw-multi.php
+++ vw-multi.php
@@ -6,7 +6,7 @@ It parses the CSV files of your bank transactions which you can
 download from your online banking account (checking and credit card).
 
 This script was created because the CSV files from Volkswagen Bank do not
-provide a separate field for the payee which make it very inconvenient for
+provide a separate field for the payee which makes them very inconvenient for
 importing into your favorite accounting software.
 
 Intended for command line use. CSV file needs to be provided as argument.
